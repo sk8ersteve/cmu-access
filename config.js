2e7993d162594a3d6493b50589494a1b60a3a05c
sk8ersteve <stephen.bulldog.thomas3@gmail.com>
2017-02-11 02:55:56 -0500

    Revert "Attempt at setting up database (but probably doesn't work)"
    This reverts commit 3b3616b926c989d4e3e509425e87556645201f30.

--- config.js
+++
@@ -1,9 +0,0 @@
- var config = {}
-
- config.host = process.env.HOST || "https://location-occupancy.documents.azure.com:443/";
- config.authKey = process.env.AUTH_KEY || "bzxl0hucWsUBlfhj2Blmrw2lTO0JvRPlFRzQhfSESlVvyiK3msvmDxIgwltxGnlyex1Fpr1BmUtSd7F3cWJCDg==";
- config.databaseId = "location-occupancy";
- config.collectionId = "locations";
-
- module.exports = config;
-
