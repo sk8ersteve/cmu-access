0b1e526d705c47e2f2c2e4d5e3d87b906419c243
sk8ersteve <stephen.bulldog.thomas3@gmail.com>
2017-02-24 15:58:28 -0500

    localStorage

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -12,7 +12,11 @@ function getLocation() {
 }
 */
 
-var inR = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+if(!localStorage.inR) {
+	localStorage.inR = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+}
+alert(localStorage.inR);
+
 
 function updateView() {
 	var restaurantNames = ["inoodle", "rothberg", "maggie", "zebra", "exchange", "cafe", "servery", "pavillion", "schatz", "abp", "gallo", "uccafe", "underground",
@@ -169,7 +173,7 @@ function check(path) {
 }
 
 function restaurants() {
-    var inoodle = [[40.443215, -79.945262], [40.443291, -79.945639], [40.443420, -79.945586], [40.443357, -79.945212]];
+    var inoodle = [[40,-80],[40,-60],[41,-60],[41,-80]];
     var rothberg = [[40.442275, -79.946124], [40.442515, -79.946048], [40.442558, -79.946240], [40.442336, -79.946359]];
     var maggie = [[40.441232, -79.943617], [40.441174, -79.943357], [40.441073, -79.943396], [40.441118, -79.943609]];
     var zebra = [[40.441723, -79.942816], [40.441733, -79.942867], [40.441589, -79.942912], [40.441588, -79.942857]];
@@ -180,24 +184,29 @@ function restaurants() {
     var schatz = [[40.443247, -79.942610], [40.443013, -79.942695], [40.442987, -79.942494], [40.443179, -79.942419]];
     var abp = [[40.444112, -79.942097], [40.444154, -79.942270], [40.443932, -79.942352], [40.443902, -79.942165]];
     var gallo = [[40.443048, -79.941964], [40.443075, -79.942169], [40.443207, -79.942115], [40.443174, -79.941922]];
-    var uccafe = [[40.445023, -79.944886],[40.444370, -79.939286],[40.447856, -79.939565],[40.447546, -79.944897]];
+    var uccafe = [[40.443207, -79.942115], [40.443174, -79.941922], [40.443278, -79.941797], [40.443329, -79.942002]];
     var underground = [[40.445373, -79.943264], [40.445432, -79.943458], [40.445241, -79.943573], [40.445191, -79.943425]];
     var laprima = [[40.442542, -79.945850], [40.442678, -79.945805], [40.442706, -79.945954], [40.442596, -79.945993]];
     var heinz = [[40.444273, -79.945369], [40.444111, -79.945443], [40.444111, -79.945641], [40.444242, -79.945756]];
     var tazza = [[40.443396, -79.944354], [40.443263, -79.944396], [40.443327, -79.944787], [40.443474, -79.944715]];
     var gingers = [[40.441148, -79.944440], [40.441208, -79.944410], [40.441222, -79.944495], [40.441169, -79.944510]];
     
-    //uc cafe coords [40.443207, -79.942115], [40.443174, -79.941922], [40.443278, -79.941797], [40.443329, -79.942002]
+    //inoodle coords [40.443215, -79.945262], [40.443291, -79.945639], [40.443420, -79.945586], [40.443357, -79.945212]
     //alert("yes");
 
     var restaurant = [inoodle, rothberg, maggie, zebra, exchange, cafe, servery, pavillion, schatz, abp, gallo, uccafe, underground,
                         laprima, heinz, tazza, gingers];
-
-
+    // Use to test check function
+	/*if (check([[40,-80],[40,-60],[41,-60],[41,-80]])) {
+		alert("true ");
+	}
+	else {
+		alert("fail");
+	}*/
     for (var i = 0; i < 17; i++) {
         var val = check(restaurant[i]);
-        if (val == true && inR[i] == 0) {
-        	inR[i] = 1;
+        if (val == true && localStorage.inR[i] == 0) {
+        	localStorage.inR[i] = 1;
         	//increment the database
         	alert("success" + i);
         	$.ajax('/api/increment/' + i, {
@@ -208,8 +217,8 @@ function restaurants() {
         	});
 
         }
-        else if (inR[i]==1) {
-			inR[i] = 0;
+        else if (val == false && localStorage.inR[i]==1) {
+			localStorage.inR[i] = 0;
         	alert("success" + i);
         	$.ajax('/api/decrement/' + i, {
             	type: 'GET',
@@ -219,6 +228,7 @@ function restaurants() {
         	});
 
         }
+        alert(localStorage.inR);
     }
     //alert(1);
     return -1;
